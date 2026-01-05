c5b6cbb1e12f2f4f2a901887794538276bf7e51a
Neo <zorthiha.neo@gmail.com>
2026-01-05 20:05:34 +0700

    Admin Panel

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,66 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Genre)
+# admin.site.register(BookInstance)
+# admin.site.register(Language)
+
+
+# catalog/admin.py
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+# ---- Author Admin ----
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    # Show these columns in the list view
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    
+    # Order fields in the edit form (dates side-by-side)
+    fields = ['first_name' , 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# ---- Book Admin ----
+# First, we need a helper method to show genres nicely in the list
+# This method MUST be added to the Book model (see Step 2 below)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    list_filter = ('author',)           # Filter sidebar by author
+    search_fields = ('title',)          # Search by book title
+    inlines = [BooksInstanceInline]
+
+# ---- BookInstance Admin ----
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    # These are the ONLY fields that exist right now
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')   # Very useful filters
+    search_fields = ('id',)
+
+    # Group fields in the edit/add form
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+# ---- Simple registration for Genre and Language ----
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -11,20 +11,46 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+# class Book(models.Model):
+#     """Model representing a book (but not a specific copy of a book)."""
+#     title = models.CharField(max_length=200)
+#     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+#     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+#     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character ISBN number')
+#     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+#     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+#     def __str__(self):
+#         return self.title
+
+#     def get_absolute_url(self):
+#         return reverse('book-detail', kwargs={'pk': self.pk})
+    
+# catalog/models.py
+
 class Book(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character ISBN number')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description')
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
+    # ←←← ADD THIS METHOD HERE (inside the class) ←←←
+    def display_genre(self):
+        """Create a string for the Genre. Used in Admin list view."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'   # Column header in admin
+
+    # Existing methods (don't remove them)
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book-detail', kwargs={'pk': self.pk})
+        return reverse('book-detail', args=[str(self.id)])
+    
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -68,7 +94,7 @@ class Author(models.Model):
         return reverse('author-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'
+        return f'{self.first_name},{self.last_name}'
 
 class Language(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
